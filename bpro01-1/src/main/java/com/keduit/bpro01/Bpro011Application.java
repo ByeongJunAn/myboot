a4afee501f39f9a660794aec7698bed75b1233cc
ByeongJunAn <abj5237@nate.com>
2023-01-19 17:47:29 +0900

    '2023-01-19'

---
+++ bpro01-1/src/main/java/com/keduit/bpro01/Bpro011Application.java
@@ -0,0 +1,13 @@
+package com.keduit.bpro01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bpro011Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bpro011Application.class, args);
+	}
+
+}
